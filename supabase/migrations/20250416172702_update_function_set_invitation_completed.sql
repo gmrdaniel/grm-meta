916a0c07c46f64c51540ba68fd2f996f367dfc73
Eliezer Castillo <eliezercastillo6@gmail.com>
2025-04-16 13:28:12 -0400

    add trigger to update invitation to completed

---
+++ supabase/migrations/20250416172702_update_function_set_invitation_completed.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION set_invitation_completed()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Update the status of the creator_invitations to 'completed'
+    UPDATE creator_invitations
+    SET status = 'completed'
+        updated_at = NOW()
+    WHERE email = NEW.email
+        AND status != 'completed';
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
