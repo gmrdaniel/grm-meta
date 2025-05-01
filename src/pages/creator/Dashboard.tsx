707a24b5c183e6ff06127fbe5d01946109f6b55c
carlos maikol <carlos.maikol1@gmail.com>
2025-05-01 15:48:08 -0400

    add dynamic background based in project invitation

--- src/pages/creator/Dashboard.tsx
+++ src/pages/creator/Dashboard.tsx
@@ -8,6 +8,7 @@ import { useAuth } from "@/hooks/useAuth";
 import { cn } from "@/lib/utils";
 import { EyeIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
 import { createClient } from "@supabase/supabase-js";
+import { fetchProjectNameByInvitationEmail } from "@/services/project/getProjectByUser";
 
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
@@ -18,6 +19,28 @@ export default function CreatorDashboard() {
   const isMobile = useIsMobile();
   const { user } = useAuth();
   const [verificationStatus, setVerificationStatus] = useState("review");
+  const [projectName, setProjectName] = useState<string>()
+
+  const getProject = async () => {
+    if(!user) return
+    if(!user.email) return
+    if(projectName != null) return
+    const name = await fetchProjectNameByInvitationEmail(user.email)
+    setProjectName(name)
+  }
+
+  const getBackgroundClass = () => {
+    if(projectName?.toUpperCase() === "PINTEREST"){
+      return 'bg-gradient-to-br from-pink-50 to-rose-100'
+    }else{
+      return ''
+    }
+    
+  }
+
+  useEffect(() => {
+    getProject()
+  }, [user])
 
   useEffect(() => {
     const fetchVerificationStatus = async () => {
@@ -45,61 +68,61 @@ export default function CreatorDashboard() {
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
         <Header />
-        <main className={cn("flex-1 overflow-y-auto p-6", isMobile && "pb-20")}>
-          <div className="max-w-7xl mx-auto space-y-6">
+        <main className={cn(`flex-1 overflow-y-auto p-6 ${getBackgroundClass()}`, isMobile && "pb-20")}>
+          <div className={`max-w-7xl mx-auto space-y-6`}>
             <div className="flex justify-between items-center">
               <h1 className="text-2xl font-semibold text-gray-800">
                 Creator Dashboard
               </h1>
             </div>
 
-            
-              {/* Tarjeta de estado de verificación */}
-              <div className="flex flex-col items-center bg-white rounded-lg shadow-md p-8 w-full max-w-md">
-                <div className="flex justify-center mb-6">
-                  <div className="bg-blue-100 rounded-full p-3">
-                    <EyeIcon className="h-8 w-8 text-blue-500" />
-                  </div>
-                </div>
-                <h2 className=" flex text-xl gap-1 font-semibold text-center text-gray-800 mb-4    whitespace-nowrap">
-                  {verificationStatus === "review" && (
-                    <>
-                     
-                      <span className="whitespace-normal md:whitespace-nowrap">
-                      Welcome! Your Submission is Under Review
-                      </span>
-                    </>
-                  )}
-                  {verificationStatus === "approved" && "You're Approved!"}
-                  {verificationStatus === "rejected" &&
-                    "Your Submission was Rejected"}
-                </h2>
-                <p className="text-center text-gray-600 mb-6 text-sm mx-auto ">
-                  {verificationStatus === "review" && (
-                    <>
-                      We've received your details and are currently verifying
-                      <br className="hidden md:block" />{" "}
-                      {/* Salto de línea en desktop */}
-                      your
-                      <br className="block md:hidden" />{" "}
-                      {/* Salto de línea solo en móvil */}
-                      information (takes 1-3 business days).
-                    </>
-                  )}
-                  {verificationStatus === "approved" &&
-                    "Congratulations! Your account has been approved and you're ready to go!"}
-                  {verificationStatus === "rejected" &&
-                    "Unfortunately, your submission was rejected. Please check your email for more details."}
-                </p>
 
-                <div className="flex items-center mt-6 gap-2">
-                  <EnvelopeIcon className="h-4 w-4 text-blue-500 sm:ml-[-15px] flex-shrink-0" />
-                  <p className="text-gray-600 sm:whitespace-nowrap whitespace-normal max-w-[250px] sm:max-w-full">
-                    You'll be notified via email/SMS once approved.
-                  </p>
+            {/* Tarjeta de estado de verificación */}
+            <div className="flex flex-col items-center bg-white rounded-lg shadow-md p-8 w-full max-w-md">
+              <div className="flex justify-center mb-6">
+                <div className="bg-blue-100 rounded-full p-3">
+                  <EyeIcon className="h-8 w-8 text-blue-500" />
                 </div>
               </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 animate-fadeIn">       
+              <h2 className=" flex text-xl gap-1 font-semibold text-center text-gray-800 mb-4    whitespace-nowrap">
+                {verificationStatus === "review" && (
+                  <>
+
+                    <span className="whitespace-normal md:whitespace-nowrap">
+                      Welcome! Your Submission is Under Review
+                    </span>
+                  </>
+                )}
+                {verificationStatus === "approved" && "You're Approved!"}
+                {verificationStatus === "rejected" &&
+                  "Your Submission was Rejected"}
+              </h2>
+              <p className="text-center text-gray-600 mb-6 text-sm mx-auto ">
+                {verificationStatus === "review" && (
+                  <>
+                    We've received your details and are currently verifying
+                    <br className="hidden md:block" />{" "}
+                    {/* Salto de línea en desktop */}
+                    your
+                    <br className="block md:hidden" />{" "}
+                    {/* Salto de línea solo en móvil */}
+                    information (takes 1-3 business days).
+                  </>
+                )}
+                {verificationStatus === "approved" &&
+                  "Congratulations! Your account has been approved and you're ready to go!"}
+                {verificationStatus === "rejected" &&
+                  "Unfortunately, your submission was rejected. Please check your email for more details."}
+              </p>
+
+              <div className="flex items-center mt-6 gap-2">
+                <EnvelopeIcon className="h-4 w-4 text-blue-500 sm:ml-[-15px] flex-shrink-0" />
+                <p className="text-gray-600 sm:whitespace-nowrap whitespace-normal max-w-[250px] sm:max-w-full">
+                  You'll be notified via email/SMS once approved.
+                </p>
+              </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 animate-fadeIn">
               {/* Tarjetas StatsCard: Se muestran solo si el usuario está aprobado */}
               {verificationStatus === "approved" && (
                 <>
