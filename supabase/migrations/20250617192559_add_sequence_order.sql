eacdcfadd57fefc4d8cba2026dcf7e0f14a0fa88
Eliezer Castillo <eliezercastillo6@gmail.com>
2025-06-24 12:28:05 -0400

    update supabase/migrations/20250617192559_add_sequence_order.sql

--- supabase/migrations/20250617192559_add_sequence_order.sql
+++ supabase/migrations/20250617192559_add_sequence_order.sql
@@ -1,1 +1,9 @@
-ALTER TABLE notification_settings ADD COLUMN sequence_order INTEGER;
+DO $$
+BEGIN
+  BEGIN
+    ALTER TABLE notification_settings ADD COLUMN sequence_order INTEGER;
+  EXCEPTION WHEN duplicate_column THEN
+    RAISE NOTICE 'column sequence_order already exists in notification_settings';
+  END;
+END;
+$$;
