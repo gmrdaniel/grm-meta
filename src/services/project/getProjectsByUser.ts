1ae6250166509bf9fd580f0a690002703ccfa8fb
Eliezer Castillo <eliezercastillo6@gmail.com>
2025-06-27 14:37:47 -0400

    change creator dashboard

---
+++ src/services/project/getProjectsByUser.ts
@@ -0,0 +1,38 @@
+import { supabase } from "@/integrations/supabase/client";
+
+type ProjectInvitation = {
+  id: string;
+  name: string;
+  invitation_link: string;
+};
+
+export const fetchProjectsByEmail = async (
+  email: string
+): Promise<ProjectInvitation[]> => {
+  if (!email) {
+    console.error("Email is null");
+    throw new Error("Email is null");
+  }
+
+  const { data, error } = await supabase
+    .from("creator_invitations")
+    .select("invitation_url,status, created_at, projects(id, name)")
+    .eq("email", email);
+
+  if (error || !data) {
+    console.error("Error fetching invitations with projects:", error);
+    throw new Error(error?.message ?? "No invitations found");
+  }
+
+  const proyectos = data
+    .filter((item) => item.projects)
+    .map((item) => ({
+      id: item.projects.id,
+      name: item.projects.name,
+      status:item.status,
+      invitation_link: item.invitation_url,
+      created_at: item.created_at, 
+    }));
+
+  return proyectos;
+};
