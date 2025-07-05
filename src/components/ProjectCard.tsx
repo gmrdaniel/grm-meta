a017614f8d04487d1c90ce47617c8075f6e112a5
Eliezer Castillo <eliezercastillo6@gmail.com>
2025-07-04 21:16:25 -0400

    fix aesthetic detail

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -44,83 +44,84 @@ export function ProjectCard({
 
   return (
     <div className="flex justify-between flex-col bg-white/80 backdrop-blur-xl p-6 rounded-2xl border border-gray-200/50 hover:shadow-lg transition-all duration-300">
-      <div className="flex items-center justify-between mb-4">
-        <span className="font-semibold text-2xl">{projectName}</span>
-        <span className="text-gray-600 bg-gray-50/50 p-2 rounded-xl">
-          <Users size={20} />
-        </span>
-      </div>
+      <div className="flex flex-col">
+        <div className="flex items-center justify-between mb-4">
+          <span className="font-semibold text-2xl">{projectName}</span>
+          <span className="text-gray-600 bg-gray-50/50 p-2 rounded-xl">
+            <Users size={20} />
+          </span>
+        </div>
 
-      <div className="text-gray-700">
-        <h3 className="text-xl mb-2">Invitations:</h3>
-        <p className="text-4xl font-bold">{totalInvitations}</p>
-        <hr className="my-4 border-t-2 border-black w-full" />
+        <div className="text-gray-700">
+          <h3 className="text-xl mb-2">Invitations:</h3>
+          <p className="text-4xl font-bold">{totalInvitations}</p>
+          <hr className="my-4 border-t-2 border-black w-full" />
 
-        <div className="flex flex-wrap mt-4">
-          <div className={statItemClass}>
-            <span className="mr-1">Pending:</span>
-            <span>
-              {pendingInvitations}{" "}
-              <small className="text-gray-500">
-                {getPercentage(pendingInvitations)}
-              </small>
-            </span>
-          </div>
-
-          <div className={statItemClass}>
-            <span className="mr-1">In process:</span>
-            <span>
-              {inProcessInvitations}{" "}
-              <small className="text-gray-500">
-                {getPercentage(inProcessInvitations)}
-              </small>
-            </span>
-          </div>
-
-          <div className={statItemClass}>
-            <span className="mr-1">Completed:</span>
-            <span>
-              {completedInvitations}{" "}
-              <small className="text-gray-500">
-                {getPercentage(completedInvitations)}
-              </small>
-            </span>
-          </div>
-
-          <div className={statItemClass}>
-            <span className="mr-1">Approved:</span>
-            <span>
-              {approvedInvitations}{" "}
-              <small className="text-gray-500">
-                {getPercentage(approvedInvitations)}
-              </small>
-            </span>
-          </div>
-
-          {acceptedInvitations > 0 && (
+          <div className="flex flex-wrap mt-4 justify-between">
             <div className={statItemClass}>
-              <span className="mr-1">Accepted:</span>
+              <span className="mr-1">Pending:</span>
               <span>
-                {acceptedInvitations}{" "}
+                {pendingInvitations}{" "}
                 <small className="text-gray-500">
-                  {getPercentage(acceptedInvitations)}
+                  {getPercentage(pendingInvitations)}
                 </small>
               </span>
             </div>
-          )}
 
-          <div className={statItemClass}>
-            <span className="mr-1">Rejected:</span>
-            <span>
-              {rejectedInvitations}{" "}
-              <small className="text-gray-500">
-                {getPercentage(rejectedInvitations)}
-              </small>
-            </span>
+            <div className={statItemClass}>
+              <span className="mr-1">In process:</span>
+              <span>
+                {inProcessInvitations}{" "}
+                <small className="text-gray-500">
+                  {getPercentage(inProcessInvitations)}
+                </small>
+              </span>
+            </div>
+
+            <div className={statItemClass}>
+              <span className="mr-1">Completed:</span>
+              <span>
+                {completedInvitations}{" "}
+                <small className="text-gray-500">
+                  {getPercentage(completedInvitations)}
+                </small>
+              </span>
+            </div>
+
+            <div className={statItemClass}>
+              <span className="mr-1">Approved:</span>
+              <span>
+                {approvedInvitations}{" "}
+                <small className="text-gray-500">
+                  {getPercentage(approvedInvitations)}
+                </small>
+              </span>
+            </div>
+
+            {acceptedInvitations > 0 && (
+              <div className={statItemClass}>
+                <span className="mr-1">Accepted:</span>
+                <span>
+                  {acceptedInvitations}{" "}
+                  <small className="text-gray-500">
+                    {getPercentage(acceptedInvitations)}
+                  </small>
+                </span>
+              </div>
+            )}
+
+            <div className={statItemClass}>
+              <span className="mr-1">Rejected:</span>
+              <span>
+                {rejectedInvitations}{" "}
+                <small className="text-gray-500">
+                  {getPercentage(rejectedInvitations)}
+                </small>
+              </span>
+            </div>
           </div>
         </div>
       </div>
-
       {inProcessInvitations > 0 && (
         <button
           onClick={handleDownload}
